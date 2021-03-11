0f414eb5c80765c9ed4a401287bc050ee458f08c
vineethgeever <vineeth.geever@gmail.com>
2021-03-11 14:40:42 +0530

    Resolved console errors

--- src/app/grid/ngz-grid.module.ts
+++ src/app/grid/ngz-grid.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { NgzGridContainerComponent } from "./components/ngz-grid-container/ngz-grid-container.component";
 
 @NgModule({
     declarations: [
         NgzGridContainerComponent
     ],
-    imports: [],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule
+    ],
     exports: [
         NgzGridContainerComponent
     ],
